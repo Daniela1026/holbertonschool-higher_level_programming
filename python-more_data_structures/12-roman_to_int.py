bdf47a1b4be00c377de82a1aee87f95eaf784e89
Daniela1026 <cartagenad49@gmail.com>
2022-09-30 08:21:31 -0700

    converts roman numeral

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or type(roman_string) != str:
+        return 0
+    converts = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    romannum = 0
+    for i in range(len(roman_string)):
+        if i > 0 and converts[roman_string[i]] > converts[roman_string[i - 1]]:
+            romannum += converts[roman_string[i]] - 2 * \
+                        converts[roman_string[i - 1]]
+        else:
+            romannum += converts[roman_string[i]]
+    return romannum
