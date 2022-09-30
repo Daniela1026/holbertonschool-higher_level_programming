8a76a96c46307ecc62f2c484d02e2a266faeff80
Daniela1026 <cartagenad49@gmail.com>
2022-09-30 09:55:29 -0700

    square defines based on 0-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def safe_print_integer(value):
-    try:
-        print("{:d}".format(value))
-    except:
-        return False
-    else:
-        return True
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
