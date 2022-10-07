591f8d015b1943dcfa64328fb0ed368f9fb7a91b
Daniela1026 <cartagenad49@gmail.com>
2022-10-06 18:57:40 -0700

    2 new lines after each of these character

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module text indentation
+"""
+
+
+def text_indentation(text):
+    """ Function that prints a text with 2 new lines """
+    flag = True
+
+    if type(text) != (str):
+        raise TypeError("text must be a string")
+
+    for i in text:
+        if i == ' ' and flag is True:
+            continue
+        if i in ['.', '?', ':']:
+            print(i, end="")
+            print()
+            print()
+            flag = True
+        else:
+            print(i, end="")
+            flag = False
