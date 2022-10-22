ba9fd362afc56690ca78d97a6665b00d8dffb046
Daniela1026 <cartagenad49@gmail.com>
2022-10-21 21:29:48 -0700

    update

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
             @staticmethod
             def to_json_string(list_dictionaries):
                 """JSON is one of the standard formats"""
-                if list_dictionaries is None or len(list_dictionaries) == 0:
-                    return "[]"
-
-                return json.dumps(list_dictionaries)
+                if list_dictionaries is None or list_dictionaries == "":
+            list_dictionaries = "[]"
+            return list_dictionaries
+        return json.dumps(list_dictionaries)
