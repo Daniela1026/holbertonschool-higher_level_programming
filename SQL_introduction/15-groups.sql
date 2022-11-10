1d831fca66b0601ffaa6779621d0ff1502a703cc
Daniela1026 <cartagenad49@gmail.com>
2022-11-10 16:26:14 -0500

    number by score

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,5 @@
 -- the number of records for this score with the label number
-SELECT score, COUNT(score) AS number FROM second_table
-GROUP BY score DESC;
+SELECT score, COUNT('score') as number
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
