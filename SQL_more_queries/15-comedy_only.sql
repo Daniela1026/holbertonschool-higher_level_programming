84410e10f46f12fddfeec64115ed79d3b03c34fa
Daniela1026 <cartagenad49@gmail.com>
2022-11-15 18:39:43 -0500

    database hbtn_0d_tvshows same as 14

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- from hbtn_0d_tvshows same as 14
+SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
