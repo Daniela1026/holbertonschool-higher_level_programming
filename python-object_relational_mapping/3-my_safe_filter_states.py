6c6f715ed1a769a52e7627de92ebff19f52e394f
Daniela1026 <cartagenad49@gmail.com>
2022-11-16 19:55:53 -0500

    an SQL injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""all values in the states table of hbtn_0e_0_usa where name
+matches the argument"""
+
+
+import sys
+import MySQLdb
+
+
+def main():
+
+    connection = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE name=%s", (
+        sys.argv[4], ))
+    records = cursor.fetchall()
+    for rec in records:
+        print(rec)
+
+        cursor.close()
+        connection.close()
+
+if __name__ == '__main__':
+    main()
