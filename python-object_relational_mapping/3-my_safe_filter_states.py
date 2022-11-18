3331e70f65cf138a26ca45b516cd26025d22088d
Daniela1026 <cartagenad49@gmail.com>
2022-11-18 10:59:22 -0500

    update

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""all values in the states table of hbtn_0e_0_usa where name
-matches the argument"""
+"""all values in the states table of hbtn_0e_0_usa"""
 
 
-import sys import argv
+import sys
 import MySQLdb
 
 
@@ -17,10 +16,14 @@ def main():
         db=sys.argv[3]
     )
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE name=%s", (argv[4], ))
+    cursor.execute("SELECT * FROM states WHERE states.name\
+       = %(username)s ORDER BY states.id", {"username": sys.argv[4]})
     records = cursor.fetchall()
     for rec in records:
         print(rec)
+
+        cursor.close()
+        connection.close()
 
         if __name__ == '__main__':
             main()
