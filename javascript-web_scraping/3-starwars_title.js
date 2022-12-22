acbe1e19b06dc635016b51f1ba5de35142ebddc8
Daniela1026 <cartagenad49@gmail.com>
2022-12-21 20:13:32 -0500

    movie title prints

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
 
-let movieid = process.argv[2];
-let url = 'http://swapi.co/api/films/' + movieid;
 const request = require('request');
-
-request(url, function (err, response, body) {
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request(url + process.argv[2], function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
-    body = JSON.parse(body);
-    console.log(body['title']);
   } else {
-    console.log('Erorr Code:' + response.statusCode);
+    console.log(JSON.parse(body).title);
   }
 });
