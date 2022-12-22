815b6142daee9f7a2867eecf2ae4f79788936f66
Daniela1026 <cartagenad49@gmail.com>
2022-12-21 20:06:06 -0500

    that prints movie title

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
-const movieid = process.argv[2];
-const url = 'http://swapi.co/api/films/' + movieid;
+let movieid = process.argv[2];
+let url = 'http://swapi.co/api/films/' + movieid;
 const request = require('request');
 
 request(url, function (err, response, body) {
