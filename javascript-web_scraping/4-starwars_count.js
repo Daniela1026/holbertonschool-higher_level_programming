f1e8ac4c8eca24c077c6c43db78ed136d0c59986
Daniela1026 <cartagenad49@gmail.com>
2022-12-23 10:28:23 -0500

    antilles wedge

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,23 @@
 #!/usr/bin/node
-
+let url = process.argv[2];
 const request = require('request');
-const url = process.argv[2];
-request.get(url, function (err, response, body) {
-  let count = 0;
+
+request(url, function (err, response, body) {
   if (err) {
     console.log(err);
-  }
-  const data = JSON.parse(body);
-  for (let i = 0; data.results[i] !== undefined; i++) {
-    if (data.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-      count++;
+  } else if (response.statusCode === 200) {
+    let films = JSON.parse(body).results;
+    let count = 0;
+    for (let i in films) {
+      let chars = films[i].characters;
+      for (let c in chars) {
+        if (chars[c].includes('18')) {
+          count++;
+        }
+      }
     }
+    console.log(count);
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
   }
-  console.log(count);
 });
